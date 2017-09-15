ac07bea91001246595a85bbc86abe80155e98a30
Melissa Santos <m.santos9@me.com>
2017-09-15 16:35:24 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
 
+// ABOUT/TABS
+$('.tab-button').click(function() {
+    var index = $(this).index();
+    $('.tab-button--active').removeClass('tab-button--active');
+    $('.tab-content--active').removeClass('tab-content--active');
+    $('.tab-content').eq(index).addClass('tab-content--active');
+    $(this).addClass('tab-button--active');
+})
+
+
+
+
+
+
+
+
+
+
