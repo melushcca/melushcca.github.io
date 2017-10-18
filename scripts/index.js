4183fb9ed22fa53e5196b43a29b7516bd49927c4
Melissa Santos <m.santos9@me.com>
2017-10-18 12:11:20 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -17,3 +17,32 @@ $(document).ready(function() {
 });
 
 
+// SMOOTH SCROLL NAVIGATION
+$('nav a[data-target]').on('click', function(event) {
+    var target = $(event.currentTarget).attr('data-target');
+    console.log('Menüpunkt wurde in der Navigation angeklickt');
+
+    //go to the section
+    var scroll_to = $('[name="' + target + '"]').offset().top;
+
+    //smoothscroll for whole site
+    $('html, body').animate({
+        scrollTop: scroll_to
+    }, 1000);
+
+ });
+
+
+// BACK TO TOP
+$(window).scroll(function() {
+    if ($(this).scrollTop()) {
+        $('#toTop').fadeIn();
+    } else {
+        $('#toTop').fadeOut();
+    }
+});
+
+$("#toTop").click(function () {
+   $("html, body").animate({scrollTop: 0}, 1000);
+   console.log('Back-To-Top');
+});
