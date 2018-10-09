12b796eec2dfbf841f777becf35df40a4986a2c2
Melissa Santos <m.santos9@me.com>
2018-10-09 10:46:04 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+require_once 'Autoloader.php';
+
+?>
+<head>
+	<meta charset="utf-8">
+	<link href="css/reset.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/mobile.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Luckiest+Guy|Open+Sans" rel="stylesheet"> 
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+</head>
+<body>
+	<header>
+    	<div class="navi">
+      		<div class="nav-img">
+        		<img src="https://www.yubico.com/wp-content/uploads/2012/09/Yubico-Logo-Big.png" alt="">
+      		</div>
+      		<nav>
+        		<ul>
+          			<li><a href="#">Neus</a></li>
+          			<li><a href="/aktuell-shop">AKTUELL IM SHOP</a></li>
+          			<li><a href="/bald-shop">BALD IM SHOP</a></li>
+        		</ul>
+      		</nav>
+    	</div>
+        <div class="header-img">
+      
+    	</div>
+  	</header>
+	<main>
+    	<h1>WILLKOMMEN</h1>
+  		<div class="news-container">
+      		<div class="teasers">
+        		<div class="first-teaser">
+          			<h3>NEUS</h3>
+        		</div>
+        		<div class="second-teaser">
+          			<div class="second-teaser-box first">
+            			<h3>AKTUELL IM SHOP</h3>
+          			</div>
+          			<div class="second-teaser-box second">
+            			<h3>BALD IM SHOP</h3>
+          			</div>
+        		</div>
+      		</div>
+      		<div class="third-teaser">
+        		<h3>neus</h3>
+      		</div>
+    	</div>
+    	<div class="search-container">
+      		<input class="main-search" type="search">
+    	</div>
+    	<div class="leaderboard-container">
+      
+    	</div>
+    </main>
+    <footer>
+    	<p>Made with _ by Melissa Santos</p>
+    </footer>
+
+</body>
